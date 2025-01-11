efb2799793b2ab051932dfc6c48f3729c8afd9b6
Heeryong Kang <drakang4@gmail.com>
2025-01-11 17:16:56 +0900

    빌드 컨픽 수정

--- apps/editor/vite.config.ts
+++ apps/editor/vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    target: 'es2023',
+  },
   plugins: [tsconfigPaths({ root: './' }), react()],
 });
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  build: {
+    target: 'es2023',
+  },
   plugins: [react(), tsconfigPaths({ root: './' })],
 });
